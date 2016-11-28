c69d73c58a77275a1d1b27f1f20ef3b2f0f3a266
Jack Phelan <phelanj@users.noreply.github.com>
2016-11-27 17:08:24 -0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+     $('.button-collapse').sideNav({
+      menuWidth: 150, // Default is 240
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
     $('.parallax').parallax();
 
   }); // end of document ready
