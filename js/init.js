1f3d74cec8926c15cd8031aca7fd761652fc86d6
Jack Phelan <phelanj@users.noreply.github.com>
2016-11-27 19:14:48 -0800

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -2,12 +2,11 @@
   $(function(){
 
      $('.button-collapse').sideNav({
-      menuWidth: 150, // Default is 240
+      menuWidth: 240, // Default is 240
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
     $('.parallax').parallax();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
