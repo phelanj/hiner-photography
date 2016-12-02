6971b6d392f86b855a5b88e2be50f3dbf896a17f
phelanj <jackphelan461@gmail.com>
2016-12-02 15:22:48 -0800

    changes

--- js/index.js
+++ js/index.js
@@ -25,4 +25,26 @@ $window.scroll(function() {
             $('.the-bass').removeClass('dropped')
         );
     });
+var header = $('.buy-btn');
+var range = 100;
+
+$(window).on('scroll', function () {
+  
+    var scrollT = $(this).scrollTop();
+    var offset = header.offset().top;
+    var height = header.outerHeight();
+    /* offset = offset + height / 2; */
+	if (scrollT >= 1) {
+    var calc = 0;
+	}
+	else calc = 1;
+
+    header.css({ 'opacity': calc });
+    if ( calc > '1' ) {
+      header.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+      header.css({ 'opacity': 0 });
+    }
+  
+});
 });
